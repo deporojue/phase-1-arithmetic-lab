ee48f7b90f9a373a8f5997e609e6a59d12fcedee
Daeyoung <daeyoungchoi@hotmail.com>
2022-01-25 22:54:06 -0800

    commit tree 3957fa538dbbde7c47dc486212530817e46e1fd7

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let multiply
+let num1 = 10
+let num2 = 62/num1
+multiply = num1 * num2
 
-// Write your code here
+console.log(multiply)
+
+let random = Math.floor(Math.random()*1000)+1
+console.log(random)
+
+let num4 = 5
+let num3 = num4 * 10 + 4
+let mod = num3 % num4
+console.log(mod)
+
+let max = Math.max(1,2,3,4,5,20)
+console.log(max)
+
+
